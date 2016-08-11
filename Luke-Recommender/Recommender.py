686429ee2e00919709b9f0d84bb7cc1ea0656f1b
yundanangel <yundanangel@outlook.com>
2016-08-12 00:03:00 +0800

    Recommender 2nd

--- Luke-Recommender/Recommender.py
+++ Luke-Recommender/Recommender.py
@@ -8,9 +8,10 @@ class DataService(object):
         cls.client=client
         cls.db=client.appstore
         cls.appinfo=cls.db.appInfo
+        cls.popular=cls.db.popular
         
     @classmethod
-    def retrieve_ratings(cls,filter_dict={}):
+    def retrieve_ratings(cls):
         rating={};
         dates={};
         rates={};
@@ -29,20 +30,28 @@ class DataService(object):
             #    rates[appInfo['appID']]=appInfo['allRating'].split(", ")[1]
         #result={rating,date,rates}
         return (rating,dates,rates)
+    @classmethod
+    def write_popular(cls,result):
+        for record in result:
+            cls.db.popular.insert(
+            {
+                "appID":record,
+            }
+            )      
 
 def main():
     try:
         client=MongoClient('localhost',27017)
         DataService.init(client)
         rating,dates,rates=DataService.retrieve_ratings()
-        popular(rating,dates,rates)
+        result=popular(rating,dates,rates)
+        DataService.write_popular(result)
         #print date
     except Exception as e:
         print(e)
         print('haha')
-    #finally:
-    #    if 'client' in locals:
-    #        client.close()
+    finally:
+        client.close()
 def popular(rating,dates,rates):
     score={}
     for appID in rating:
@@ -58,8 +67,27 @@ def popular(rating,dates,rates):
         print diff
         num=int(rates[appID])
         score[appID]=num/diff*r
-        
-        
+    result= sorted(score.iteritems(), key=lambda d:d[1], reverse = True)[0:11]
+    print result
+    return result
+def Nice(rating,dates,rates):
+    score={}
+    for appID in rating:
+        #print rating[appID]
+        r=float(rating[appID])
+        year=int(dates[appID].split(', ')[1])
+        month=dates[appID].split(', ')[0].split(' ')[0]
+        rmonth=strptime(month,'%b').tm_mon
+        day=int(dates[appID].split(', ')[0].split(' ')[1])
+        today = date.today()
+        someday = date(year, rmonth, day)
+        diff=(today-someday).days
+        print diff
+        num=int(rates[appID])
+        score[appID]=num/diff*r
+    result= sorted(score.iteritems(), key=lambda d:d[1], reverse = True)[0:11]
+    print result
+    return result      
         
 
         
