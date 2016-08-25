aca0a8f617de44b4e2f45ad6d1f8ee8a2d5a9179
DereckHan <pitthhy@gmail.com>
2016-08-25 15:19:20 -0400

    update banner

--- MongoDBdemo/src/main/java/com/example/controller/AppController.java
+++ MongoDBdemo/src/main/java/com/example/controller/AppController.java
@@ -18,7 +18,7 @@ public class AppController {
     AppMongoRepoCus appRepositoryCus;
 
     //Main Page
-    @RequestMapping("main")
+    @RequestMapping("/main")
     public String mainPage(Model model) {
         model.addAttribute("top100", appRepositoryCus.top100Apps());
         model.addAttribute("popular24", appRepositoryCus.popular24Apps());
@@ -37,19 +37,19 @@ public class AppController {
     public String popular24(@PathVariable String category, Model model) {
         model.addAttribute("top100", appRepositoryCus.top100Apps());
         model.addAttribute("popular24", appRepositoryCus.popular24Apps());
-        int startIndex=0;
-        if (category.equals("Entertainment")) startIndex=0;
-        if (category.equals("Games")) startIndex=1;
-        if (category.equals("Health_Fitness")) startIndex=2;
-        if (category.equals("News")) startIndex=3;
-        if (category.equals("Productivity")) startIndex=4;
-        if (category.equals("Photo_Video")) startIndex=5;
-        if (category.equals("Music")) startIndex=6;
-        if (category.equals("Shopping")) startIndex=7;
-        if (category.equals("Social_Networking")) startIndex=8;
-        if (category.equals("Utilities")) startIndex=9;
+        int startIndex = 0;
+        if (category.equals("Entertainment")) startIndex = 0;
+        if (category.equals("Games")) startIndex = 1;
+        if (category.equals("Health_Fitness")) startIndex = 2;
+        if (category.equals("News")) startIndex = 3;
+        if (category.equals("Productivity")) startIndex = 4;
+        if (category.equals("Photo_Video")) startIndex = 5;
+        if (category.equals("Music")) startIndex = 6;
+        if (category.equals("Shopping")) startIndex = 7;
+        if (category.equals("Social_Networking")) startIndex = 8;
+        if (category.equals("Utilities")) startIndex = 9;
         model.addAttribute("startIndex", startIndex);
-        model.addAttribute("category",category);
+        model.addAttribute("category", category);
         return "popular24";
     }
 
@@ -67,7 +67,7 @@ public class AppController {
     }
 
     //show all apps
-    @RequestMapping("/app")
+    @RequestMapping("/apps")
     public String app(Model model) {
         model.addAttribute("appList", appRepositoryCus.searchByCategory("Books"));
         return "app";
@@ -75,8 +75,8 @@ public class AppController {
 
 
     //show all apps from specific category
-    @RequestMapping("/app/{category}")
-    public String appByCategory(@PathVariable String category, Model model) {
+    @RequestMapping("/apps/{category}/{page}")
+    public String appByCategory(@PathVariable String category, @PathVariable int page, Model model) {
         model.addAttribute("category", category);
         if (category.equals("Food_Drink")) category = "Food & Drink";
         if (category.equals("Health_Fitness")) category = "Health & Fitness";
@@ -84,6 +84,20 @@ public class AppController {
         if (category.equals("Magazines_Newspapers")) category = "Magazines & Newspapers";
         if (category.equals("Social_Networking")) category = "Social Networking";
         model.addAttribute("appList", appRepositoryCus.searchByCategory(category));
+        model.addAttribute("pre", page - 1);
+        model.addAttribute("start", (page - 1) * 60);
+        model.addAttribute("page", page);
+        model.addAttribute("next", page);
+        int pageNumber;
+        if (appRepositoryCus.searchByCategory(category).size() / 60 * 60 == appRepositoryCus.searchByCategory(category).size())
+            pageNumber = appRepositoryCus.searchByCategory(category).size() / 60;
+        else
+            pageNumber = 1 + appRepositoryCus.searchByCategory(category).size() / 60;
+        model.addAttribute("pageNumber", pageNumber);
+        if (page == pageNumber)
+            model.addAttribute("end", appRepositoryCus.searchByCategory(category).size() - 1);
+        else
+            model.addAttribute("end", page*60-1);
         return "appCategory";
     }
 
@@ -92,7 +106,11 @@ public class AppController {
     public String showApp(@PathVariable String appID, String category, Model model) {
         AppDocument document = appRepositoryCus.searchById(appID);
         model.addAttribute("app", document);
-        List<String> screenShots = document.getiPhone_screenShot();
+        if (!document.getiPhone_screenShot().isEmpty()) {
+            model.addAttribute("screenShots", document.getiPhone_screenShot());
+        } else if (!document.getiPad_screenShot().isEmpty()) {
+            model.addAttribute("screenShots", document.getiPad_screenShot());
+        }
         List<String> description = document.getDescription();
         List<AppDocument> relatedApps = appRepositoryCus.findRelatedApps(appID);
         List<AppDocument> categoryList = appRepositoryCus.searchByCategory(document.getCategory());
@@ -104,11 +122,7 @@ public class AppController {
         }
         model.addAttribute("relatedAppsList", relatedApps);
         model.addAttribute("description", description);
-        if (!screenShots.isEmpty()) {
-            model.addAttribute("screenShots", screenShots);
-            return "appShow";
-        } else
-            return "test";
+        return "appShow";
     }
 
 }
